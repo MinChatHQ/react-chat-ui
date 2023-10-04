d18ba650d9a2460a712e6ac7c4ee78d440317cc7
Kaputo Kalandanya <kaputokalan11@gmail.com>
2023-10-04 15:05:52 +0200

    No selected conversations gray view
    Added a gray view when yeah no conversations are selected

--- src/main-container/index.tsx
+++ src/main-container/index.tsx
@@ -1,115 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import useCheckIsMobile from '../hooks/useCheckIsMobile';
 import Loading from '../loading';
-import MessageContainer, { Props as MessageContainerProps } from '../message-container';
-import ConversationContainer, { Props as ConversationContainerProps } from '../conversation-container';
-import "../index.css"
-
+import MessageContainer, {
+  Props as MessageContainerProps,
+} from '../message-container';
+import ConversationContainer, {
+  Props as ConversationContainerProps,
+} from '../conversation-container';
+import '../index.css';
 
 const Container = styled.div`
-    height: 100%;
-    position: relative;
-`
+  height: 100%;
+  position: relative;
+`;
 
 const DesktopContainer = styled.div`
-    display: flex;
-    height: 100%;
-    width: 100%;
-
-`
-
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
 
 const Sidebar = styled.div`
-    /* max-width: 384px; */
-    padding-top: 16px;
-    padding-bottom: 16px;
-    width: 35%;
-    height: 100%;
-    position: relative;
-    box-sizing: border-box;
-
-`
+  /* max-width: 384px; */
+  padding-top: 16px;
+  padding-bottom: 16px;
+  width: 35%;
+  height: 100%;
+  position: relative;
+  box-sizing: border-box;
+`;
 
 const DesktopContent = styled.div`
-    height: 100%;
-    width: 65%;
-    position: relative;
-    
-
-`
+  height: 100%;
+  width: 65%;
+  position: relative;
+`;
+const EmptyDesktopContent = styled.div`
+  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+  position: relative;
+  background-color: #f3f4f6;
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export interface Props {
-    mobileView?: boolean
-    inbox: ConversationContainerProps
-    selectedConversation?: MessageContainerProps
+  mobileView?: boolean;
+  inbox: ConversationContainerProps;
+  selectedConversation?: MessageContainerProps;
 }
 
 /**
- * 
+ *
  * a chat view that automatically switches between desktop view and mobile view
- * @returns 
+ * @returns
  */
 export default function MainContainer({
-    mobileView,
-    inbox,
-    selectedConversation
+  mobileView,
+  inbox,
+  selectedConversation,
 }: Props) {
+  const [useMobileView, setUseMobileView] = useState<boolean>(true);
 
-    const [useMobileView, setUseMobileView] = useState<boolean>(true)
+  const isMobile = useCheckIsMobile();
 
-    const isMobile = useCheckIsMobile()
+  useEffect(() => {
+    if (mobileView === true) {
+      setUseMobileView(true);
+    } else if (mobileView === false) {
+      setUseMobileView(false);
+    } else {
+      setUseMobileView(isMobile);
+    }
+  }, [isMobile, mobileView]);
 
-    useEffect(() => {
-        if (mobileView === true) {
-            setUseMobileView(true)
-        } else if (mobileView === false) {
-            setUseMobileView(false)
-        } else {
-            setUseMobileView(isMobile)
-        }
+  return (
+    <Container>
+      {useMobileView ? (
+        selectedConversation ? (
+          <MessageContainer {...selectedConversation} mobileView={mobileView} />
+        ) : (
+          <>
+            <ConversationContainer {...inbox} />
+          </>
+        )
+      ) : (
+        <DesktopContainer>
+          {inbox.loading ? (
+            <Loading themeColor={inbox.themeColor} />
+          ) : (
+            <>
+              <Sidebar>
+                <ConversationContainer {...inbox} showHeader={false} />
+              </Sidebar>
 
-    }, [isMobile, mobileView])
-
-    return (
-        <Container>
-            {useMobileView ? (
-                selectedConversation ?
-                    <MessageContainer
-                        {...selectedConversation}
-                        mobileView={mobileView}
-                    />
-                    :
-                    <ConversationContainer
-                        {...inbox}
-                    />
-            )
-                :
-                <DesktopContainer>
-                    {inbox.loading ?
-
-                        <Loading
-                            themeColor={inbox.themeColor} />
-
-                        :
-                        <>
-                            <Sidebar>
-                                <ConversationContainer
-                                    {...inbox}
-                                    showHeader={false}
-                                />
-                            </Sidebar>
-
-                            <DesktopContent>
-                                {selectedConversation &&
-                                    <MessageContainer
-                                        showBack={false}
-                                        {...selectedConversation}
-                                    />}
-                            </DesktopContent>
-                        </>}
-                </DesktopContainer>
-            }
-        </Container>
-    )
+              <DesktopContent>
+                {selectedConversation ? (
+                  <MessageContainer
+                    showBack={false}
+                    {...selectedConversation}
+                  />
+                ) : (
+                  <>
+                    <EmptyDesktopContent />
+                  </>
+                )}
+              </DesktopContent>
+            </>
+          )}
+        </DesktopContainer>
+      )}
+    </Container>
+  );
 }
