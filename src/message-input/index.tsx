f7520c0a525e0a71f97791843821e9b052d8dff6
Mundia <fxmundia416@gmail.com>
2023-10-21 12:02:20 +0200

    cleanup

--- src/message-input/index.tsx
+++ src/message-input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, {  useRef, useState } from 'react'
 import styled from 'styled-components'
 import useTypingListener from '../hooks/useTypingListener'
 
