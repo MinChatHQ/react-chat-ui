7195bd150b2486a6d817b1b15040b61cdf5d11d7
Mundia <fxmundia416@gmail.com>
2025-06-16 13:51:51 +0200

    updated build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
     plugins: [
         react(),
-        dts({ tsconfigPath: './tsconfig.app.json' })
+        dts({ tsconfigPath: './tsconfig.app.json' }),
+        cssInjectedByJsPlugin()
     ],
     build: {
         lib: {
